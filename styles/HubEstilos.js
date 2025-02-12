732a922ccfd7a534eb7b376d71e0192f9dd5778d
marcosdogexd <marcosdoge3@gmail.com>
2025-02-12 02:38:22 -0500

    Cambios

--- styles/HubEstilos.js
+++ styles/HubEstilos.js
@@ -1,7 +1,15 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
+    
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
       flex: 1,
       alignItems: "center",
       justifyContent: "center",
